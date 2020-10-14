233565cf0ffc4efa814fe0482b2150a1147b63c7
Wiktor Gradowski <Wiwi.gradowski@outlook.com>
2020-10-14 19:41:04 +0200

    Komunikat

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String tekst = "tekst";
+            Console.WriteLine(tekst);
+            tekst = Console.ReadLine();
+            Console.WriteLine(tekst);
+            Console.ReadLine();
+            //Oczeiwanie na kliknięcie
+            //konsole.ReadLine();
+            Main(args);
+        }
+    }
+}
